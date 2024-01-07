f969640eb52a7e36d730f175851dbcb95e3e44e9
Aliyev Davud <davudva@code.local>
2024-01-07 20:02:48 +0400

    project created

---
+++ quiz1/quiz1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace quiz1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+        //Verilmiş sözün tərsdən oxunuşunun özü ilə eyni olub olmadığnı tapan metod
+        static string GivenWord(string word)
+        {
+            string text = "";
+
+            for (int i=0 ; i < word.Length-1; i++)
+            {
+                text+= word[i];
+            }
+            
+            
+            if (word ==text )
+            {
+                Console.WriteLine("Eynidir");
+            }
+            else
+            {
+                Console.WriteLine("Eyni deyil");
+            }
+        }
+
+        //Verilmiş array-dən verilmiş indexli elementi silən metod.
+
+        static string DeleteGivenOne(string[] text)
+        {
+
+        }
+
+        //Verilmiş yazının əvvəlindəki boşluqları silən metod
+
+        static string DeleteSpace(string text)
+        {
+            string text1 = "";
+
+        }
+
+    }
+}
